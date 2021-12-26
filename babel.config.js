8f028d29b69ca84d9ee255b16a5d10bc608dd402
danielterletzkiy <danielterletzkiy@gmail.com>
2021-12-26 06:15:49 +0100

    removed old build script

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,8 @@
-const devPresets = ["@babel/preset-env", {
-    "useBuiltIns": false,
-}];
+const devPresets = ["@babel/preset-env"];
 const buildPresets = ['@babel/preset-env'];
+
+const devPlugins = ["@babel/plugin-transform-runtime"]
 module.exports = {
     presets: (process.env.NODE_ENV === 'development' ? devPresets : buildPresets),
+    plugins: (process.env.NODE_ENV === 'development' ? devPlugins : [])
 };
