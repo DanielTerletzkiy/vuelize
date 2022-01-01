6ade9c8703c99a6834e900396aba8b4cea904383
danielterletzkiy <danielterletzkiy@gmail.com>
2022-01-02 00:50:21 +0100

    better babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,12 @@
-const devPresets = ["@babel/preset-env"];
-const buildPresets = ['@babel/preset-env'];
-
-const devPlugins = ["@babel/plugin-transform-runtime"]
 module.exports = {
-    presets: (process.env.NODE_ENV === 'development' ? devPresets : buildPresets),
-    plugins: (process.env.NODE_ENV === 'development' ? devPlugins : devPlugins)
-};
+    presets: [
+        [
+            '@babel/preset-env',
+            {
+                targets: {
+                    esmodules: true,
+                },
+            },
+        ],
+    ],
+}
