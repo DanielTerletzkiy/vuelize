127584cb43ce778804526eeb99cebaf0357a5b9c
danielterletzkiy <danielterletzkiy@gmail.com>
2022-01-23 04:20:03 +0100

    adding gh-pages to subtree

---
+++ src/components/chart/Chart.vue
@@ -0,0 +1,36 @@
+<template>
+  <d-function-wrapper :classes="['d-chart']" v-bind="{...$props, ...$attrs}">
+    <d-row>
+      <d-column>
+        <d-card-subtitle v-for="i in Array.from(Array(10).keys())" :key="i">
+          {{ i }}
+        </d-card-subtitle>
+      </d-column>
+      <d-divider block vertical/>
+      <d-column>
+        chart
+        <d-spacer/>
+        <d-divider block/>
+        <d-row>
+          <d-card-subtitle v-for="i in Array.from(Array(10).keys())" :key="i">
+            {{ i }}
+          </d-card-subtitle>
+        </d-row>
+      </d-column>
+    </d-row>
+  </d-function-wrapper>
+</template>
+
+<script>
+export default {
+  name: "d-chart",
+
+  props: {
+    variant: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
