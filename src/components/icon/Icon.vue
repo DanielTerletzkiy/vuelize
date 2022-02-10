59754889f172ac0fe8cc192c53f5bb59e881a8fb
danielterletzkiy <danielterletzkiy@gmail.com>
2022-02-10 23:59:01 +0100

    icon gradient

--- src/components/icon/Icon.vue
+++ src/components/icon/Icon.vue
@@ -1,5 +1,14 @@
 <template>
-  <unicon class="d-icon unicon--large" :name="name" :width="size" :height="size" :icon-style="iconStyle" :fill="this.processColor(color || 'currentColor')"></unicon>
+  <div>
+    <svg style="width:0;height:0;position:absolute;" aria-hidden="true" focusable="false">
+      <linearGradient :id="this.uuid" x1="0%" y1="0%" x2="100%" y2="0%" :gradientTransform="`rotate(${gradientRotation})`" v-if="gradient">
+        <stop v-for="{offset, color} in gradient" :key="offset" :offset="offset+'%'"
+              :stop-color="processColor(color || 'currentColor')"/>
+      </linearGradient>
+    </svg>
+    <unicon class="d-icon unicon--large" :name="name" :width="size" :height="size" :icon-style="iconStyle"
+            :fill="`url(#${this.uuid}) ${this.processColor(color || 'currentColor')}`" @click="$emit('click')"></unicon>
+  </div>
 </template>
 
 <script>
@@ -8,9 +17,10 @@ export default {
 
   props: {
     name: {type: String, required: true},
-    color: String,
     iconStyle: String,
     size: [String, Number],
+    gradient: Array,
+    gradientRotation: {type: [String, Number], default: 0},
   }
 }
 </script>
