587585061487b15a14b988c07435fce19e288fb7
daniel.terletzkiy <daniel.terletzkiy@check24.de>
2022-11-14 09:42:04 +0100

    VEZ-1 image fix

---
+++ docs/src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import Vuelize from "../../src/index";
+const app = createApp(App);
+//@ts-ignore
+app.use(Vuelize);
+app.mount('#app');
+//# sourceMappingURL=main.js.map
