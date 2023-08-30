a0a3cbfce0f56089b7cf4e154dfae5f2db7f1410
daniel.terletzkiy <daniel.terletzkiy@check24.de>
2023-08-30 09:30:21 +0200

    toggle focus

--- src/components/toggle/DToggle.vue
+++ src/components/toggle/DToggle.vue
@@ -5,6 +5,8 @@
     v-bind="{...$props, ...$attrs}"
     role="toggle"
     :outlined="{color: currentState&&coloredOutline&&currentState.color, transparency: 60,width: '2px'}"
+    :tabindex="disabled || !simpleToggle?-1:0"
+    @keyup.enter="simpleToggle && simpleClick()"
   >
     <d-row
       ref="parent"
@@ -75,23 +77,23 @@ defineExpose({wrapper});
 const emit = defineEmits(['update:modelValue', 'toggleValue'])
 
 const props = defineProps({
-    modelValue: {type: Number, required: true},
-    states: {
-        type: Array as PropType<ToggleState[]>, default: () => [
-            {
-                color: ThemeColorProperty.error,
-                icon: 'times',
-                value: false,
-            }, {
-                color: ThemeColorProperty.success,
-                icon: 'check',
-                value: true,
-            }
-        ]
-    },
-    size: {type: Number, default: 30},
-    coloredOutline: {type: Boolean},
-    ...defaultProps
+  modelValue: {type: Number, required: true},
+  states: {
+    type: Array as PropType<ToggleState[]>, default: () => [
+      {
+        color: ThemeColorProperty.error,
+        icon: 'times',
+        value: false,
+      }, {
+        color: ThemeColorProperty.success,
+        icon: 'check',
+        value: true,
+      }
+    ]
+  },
+  size: {type: Number, default: 30},
+  coloredOutline: {type: Boolean},
+  ...defaultProps
 });
 
 const sizePX = computed(() => `${props.size}px`)
@@ -101,11 +103,11 @@ const simpleToggle = computed(() => filteredStates.value.length < 3)
 const hovering = ref<number>(-1);
 
 function onHover(index: number) {
-    hovering.value = index;
+  hovering.value = index;
 }
 
 function onHoverLeave() {
-    hovering.value = -1;
+  hovering.value = -1;
 }
 
 const filteredStates = computed(() => props.states.filter((state: ToggleState) => !state.hidden))
@@ -113,11 +115,11 @@ const filteredStates = computed(() => props.states.filter((state: ToggleState) =
 const currentState = computed<ToggleState | null>(() => props.states.at(props.modelValue));
 
 function setCurrent(index: number) {
-    emit('update:modelValue', index);
+  emit('update:modelValue', index);
 }
 
 function simpleClick() {
-    setCurrent(props.modelValue === 0 ? 1 : 0);
+  setCurrent(props.modelValue === 0 ? 1 : 0);
 }
 
 const parent = ref<ComponentPublicInstance<HTMLElement> | null>(null)
@@ -127,44 +129,44 @@ const stateButtons = ref<ComponentPublicInstance<HTMLElement>[]>()
 watch([() => props.modelValue, sizePX], updateIndicator)
 
 function updateIndicator() {
-    nextTick(() => {
-        emit('toggleValue', currentState.value?.value);
-    })
-    if (props.modelValue === undefined || props.modelValue < 0) {
-        return;
-    }
-    const button = stateButtons.value?.at(props.modelValue);
-    if (!button || !indicator.value || !parent.value) {
-        return;
-    }
-    const parentRect: Partial<DOMRect> = getBoundingClientRect(parent.value.$el);
-    const childRect: Partial<DOMRect> = getBoundingClientRect(button.$el);
+  nextTick(() => {
+    emit('toggleValue', currentState.value?.value);
+  })
+  if (props.modelValue === undefined || props.modelValue < 0) {
+    return;
+  }
+  const button = stateButtons.value?.at(props.modelValue);
+  if (!button || !indicator.value || !parent.value) {
+    return;
+  }
+  const parentRect: Partial<DOMRect> = getBoundingClientRect(parent.value.$el);
+  const childRect: Partial<DOMRect> = getBoundingClientRect(button.$el);
 
-    const rect: Partial<DOMRect> = {
-        height: 0,
-        width: 0,
-        x: 0,
-        y: 0,
-        bottom: 0,
-        left: 0,
-        right: 0,
-        top: 0,
-    };
-    Object.entries(childRect).forEach(([key, value]) => {
-        const rectKey = key as keyof DOMRect;
-        const parentValue = parentRect[rectKey] as number;
+  const rect: Partial<DOMRect> = {
+    height: 0,
+    width: 0,
+    x: 0,
+    y: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    top: 0,
+  };
+  Object.entries(childRect).forEach(([key, value]) => {
+    const rectKey = key as keyof DOMRect;
+    const parentValue = parentRect[rectKey] as number;
 
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        //@ts-ignore
-        rect[rectKey] = value - parentValue
-    });
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    //@ts-ignore
+    rect[rectKey] = value - parentValue
+  });
 
-    indicator.value.wrapper.wrapper.style.setProperty('left', `${rect.left}px`);
+  indicator.value.wrapper.wrapper.style.setProperty('left', `${rect.left}px`);
 }
 
 function getBoundingClientRect(element: HTMLElement) {
-    const {top, right, bottom, left, width, height, x, y} = element.getBoundingClientRect();
-    return {top, right, bottom, left, width, height, x, y}
+  const {top, right, bottom, left, width, height, x, y} = element.getBoundingClientRect();
+  return {top, right, bottom, left, width, height, x, y}
 }
 
 onMounted(() => nextTick().then(updateIndicator))
@@ -174,24 +176,29 @@ onMounted(() => nextTick().then(updateIndicator))
 <style lang="scss">
 
 .d-toggle {
-    position: relative;
-    width: max-content;
-    overflow: hidden;
-    cursor: pointer;
+  position: relative;
+  width: max-content;
+  overflow: hidden;
+  cursor: pointer;
 
-    &__indicator {
-        position: absolute !important;
-        top: 0;
-        left: 0;
-        background: black;
-        border-radius: inherit;
-        transition: all .4s;
-        pointer-events: none;
-    }
+  &__indicator {
+    position: absolute !important;
+    top: 0;
+    left: 0;
+    background: black;
+    border-radius: inherit;
+    transition: all .4s;
+    pointer-events: none;
+  }
 
-    /*&:focus-within {
-        outline: 2px solid var(--sheet-secondary);
-        outline-offset: 2px;
-    }*/
+  &:focus-visible {
+    outline: 2px solid currentColor !important;
+    outline-offset: 2px;
+  }
+
+  /*&:focus-within {
+      outline: 2px solid var(--sheet-secondary);
+      outline-offset: 2px;
+  }*/
 }
 </style>
