e3245b91411d5a2bb5989cbbffb44f54430c5c63
daniel.terletzkiy <daniel.terletzkiy@check24.de>
2023-09-28 13:18:49 +0200

    fixed height and colors

--- src/components/textfield/variant/DSelect.vue
+++ src/components/textfield/variant/DSelect.vue
@@ -34,6 +34,7 @@
       rounded="md"
       class="d-text-field__input__icon"
       tabindex="-1"
+      type="button"
       @click="toggleDropdown"
     >
       <TransitionSlide
@@ -80,16 +81,16 @@ import defaultProps from "../../../props/default.props";
 
 const emit = defineEmits(['update:modelValue', 'removeFocus', 'focusout', 'focusin'])
 const props = defineProps({
-    modelValue: {
-        type: [Number, String],
-        required: true,
-    },
-    items: {type: Array, required: true},
-    indexKey: {type: String},
-    mandatory: {type: Boolean},
-    search: {type: Boolean},
-    searchKey: {type: String, default: 'value'},
-    ...defaultProps
+  modelValue: {
+    type: [Number, String],
+    required: true,
+  },
+  items: {type: Array, required: true},
+  indexKey: {type: String},
+  mandatory: {type: Boolean},
+  search: {type: Boolean},
+  searchKey: {type: String, default: 'value'},
+  ...defaultProps
 })
 
 const itemsCopy = ref(props.items);
@@ -101,53 +102,53 @@ watch(searchInput, mapItems)
 watch(() => props.items, mapItems)
 
 function mapItems() {
-    itemsCopy.value = props.items;
-    if (!itemsCopy.value || !props.items) {
-        return;
+  itemsCopy.value = props.items;
+  if (!itemsCopy.value || !props.items) {
+    return;
+  }
+  itemsCopy.value = props.items.map((item: any) => {
+    if (!item[props.searchKey] || !searchInput.value.toLowerCase()) {
+      return {...item, _show: true};
     }
-    itemsCopy.value = props.items.map((item: any) => {
-        if (!item[props.searchKey] || !searchInput.value.toLowerCase()) {
-            return {...item, _show: true};
-        }
-        item = {
-            ...item,
-            _show: item[props.searchKey].toString().toLowerCase().includes(searchInput.value.toLowerCase())
-        };
-        return item;
-    });
+    item = {
+      ...item,
+      _show: item[props.searchKey].toString().toLowerCase().includes(searchInput.value.toLowerCase())
+    };
+    return item;
+  });
 }
 
 const dropdownOpen = ref(false);
 
 function toggleDropdown() {
-    dropdownOpen.value = !dropdownOpen.value;
+  dropdownOpen.value = !dropdownOpen.value;
 }
 
 watch(dropdownOpen, (value) => {
-    if (value) {
-        nextTick(() => {
-            if (searchBox.value) {
-                searchBox.value.focus();
-            }
-        })
-    }
+  if (value) {
+    nextTick(() => {
+      if (searchBox.value) {
+        searchBox.value.focus();
+      }
+    })
+  }
 })
 
 function onInput(val: number | string) {
-    emit('update:modelValue', val)
-    emit('removeFocus')
+  emit('update:modelValue', val)
+  emit('removeFocus')
 }
 
 function focusOut() {
-    emit('focusout');
+  emit('focusout');
 }
 
 function focusIn() {
-    emit('focusin');
+  emit('focusin');
 }
 
 const angleIcon = computed(() => {
-    return dropdownOpen.value ? 'times' : 'angle-down'
+  return dropdownOpen.value ? 'times' : 'angle-down'
 })
 
 onBeforeMount(mapItems)
@@ -155,36 +156,36 @@ onBeforeMount(mapItems)
 
 <style scoped lang="scss">
 :deep(.d-select-menu__dropdown) {
-    left: -1.2em;
-    width: calc(100% + (1.2em * 2));
+  left: -1.2em;
+  width: calc(100% + (1.2em * 2));
 }
 
-.d-select{
-  height: 100%;
+.d-select {
+  height: 100% !important;
 
-  &-label{
+  &-label {
     width: 100%;
     display: flex;
   }
 }
 
 .d-text-field__input {
-    cursor: pointer;
-    user-select: none;
-    position: relative;
-    margin-top: 0.5em !important;
-    margin-bottom: 0.5em !important;
-    display: flex;
+  cursor: pointer;
+  user-select: none;
+  position: relative;
+  margin-top: 0.5em !important;
+  margin-bottom: 0.5em !important;
+  display: flex;
 
-    &__search {
-        margin-left: 0 !important;
-        cursor: text;
-    }
+  &__search {
+    margin-left: 0 !important;
+    cursor: text;
+  }
 
-    &__icon {
-        margin-left: auto;
-        margin-right: -8px;
-        align-self: center;
-    }
+  &__icon {
+    margin-left: auto;
+    margin-right: -8px;
+    align-self: center;
+  }
 }
 </style>
